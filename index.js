b0c852da81bd398ca8ee1d82cc8b0460d6b072d9
aharbison <arh79@georgetown.edu>
2015-06-20 01:05:09 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var contents = fs.readFileSync('index.html');
+var stringContents = buf.toString(contents); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+    response.send(stringContents);
 })
 
 app.listen(app.get('port'), function() {
